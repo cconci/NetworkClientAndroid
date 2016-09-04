bc5a2c4048d9ec3ecd1f7424ccbb6b865904b30f
cconci <cconci.dev@gmail.com>
2016-09-04 10:16:27 +1000

    Initial commit - Quick UI knock up

---
+++ app/src/main/java/com/cconci/networkclient/NetworkClientMainActivity.java
@@ -0,0 +1,13 @@
+package com.cconci.networkclient;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NetworkClientMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_network_client_main);
+    }
+}
